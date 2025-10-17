f6834c0505f2ecb1af3dd57aa0520c3b640c441b
SYJ1029 <abg0986@gmail.com>
2025-10-17 15:05:01 +0900

    프로젝트 생성

---
+++ GLBlockJump/GLBlockJump/vertex.glsl
@@ -0,0 +1,23 @@
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal; // 정점 노말 추가
+layout(location = 2) in vec3 color;
+
+out vec3 fragColor;
+out vec3 FragPos;
+out vec3 Normal;
+
+uniform mat4 modelTransform;
+uniform mat4 viewTransform;
+uniform mat4 projectionTransform;
+uniform mat3 normalMatrix; // main.cpp에서 전달된 normalMatrix
+
+void main()
+{
+    // Transform positions
+    FragPos = vec3(modelTransform * vec4(position, 1.0));
+    Normal = normalize(normalMatrix * normal);
+    fragColor = color; // 정점 색상 전달
+    gl_Position = projectionTransform * viewTransform * modelTransform * vec4(position, 1.0);
+}
