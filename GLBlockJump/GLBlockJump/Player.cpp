33810acd4cc97afc8a5647fa8ba5b88bde8ecd0d
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-11-01 23:08:30 +0900

    자유낙하 추가
    Player Update() 구현 완

--- GLBlockJump/GLBlockJump/Player.cpp
+++ GLBlockJump/GLBlockJump/Player.cpp
@@ -8,6 +8,7 @@ void Player::Init(float newPos[3])
 
 void Player::Update()
 {
+    //전후좌우 이동
     glm::mat4 rotMat = glm::rotate(glm::mat4(1.0f), glm::radians(-rotation[1]), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 right = glm::vec3(1.0f, 0.0f, 0.0f);
@@ -31,10 +32,15 @@ void Player::Update()
         pos[2] -= right.z * moveSpeed[2];
     }
 
+    //점프
     if (inputs.jump && isGrounded) {
         isGrounded = false;
         moveSpeed[1] = jumpSpeed;
 	}
+
+    //자유낙하
+    moveSpeed[1] -= GRAVITY;
+	pos[1] += moveSpeed[1];
 }
 
 void Player::Release()
