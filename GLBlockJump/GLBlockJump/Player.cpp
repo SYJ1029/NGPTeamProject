b6b239a35b277378ffc65d248109a288d97e6a2b
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-11-01 23:00:28 +0900

    플레이어 전후좌우 이동 추가

--- GLBlockJump/GLBlockJump/Player.cpp
+++ GLBlockJump/GLBlockJump/Player.cpp
@@ -8,7 +8,28 @@ void Player::Init(float newPos[3])
 
 void Player::Update()
 {
+    glm::mat4 rotMat = glm::rotate(glm::mat4(1.0f), glm::radians(-rotation[1]), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 right = glm::vec3(1.0f, 0.0f, 0.0f);
+    forward = glm::normalize(glm::vec3(rotMat * glm::vec4(forward, 0.0f)));
+    right = glm::normalize(glm::vec3(rotMat * glm::vec4(right, 0.0f)));
 
+    if (inputs.updown == 1) {
+        pos[0] -= forward.x * moveSpeed[0];
+        pos[2] -= forward.z * moveSpeed[2];
+    }
+    else if (inputs.updown == -1) {
+        pos[0] += forward.x * moveSpeed[0];
+        pos[2] += forward.z * moveSpeed[2];
+    }
+    if (inputs.rightleft == -1) {
+        pos[0] += right.x * moveSpeed[0];
+        pos[2] += right.z * moveSpeed[2];
+    }
+    else if (inputs.rightleft == 1) {
+        pos[0] -= right.x * moveSpeed[0];
+        pos[2] -= right.z * moveSpeed[2];
+    }
 }
 
 void Player::Release()
