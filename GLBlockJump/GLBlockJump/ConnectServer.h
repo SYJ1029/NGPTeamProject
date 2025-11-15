4b2ad8f1f631d0a11aa0ab0df34d24381274abd2
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-11-16 01:18:06 +0900

    SendInputChange() 생성

--- GLBlockJump/GLBlockJump/ConnectServer.h
+++ GLBlockJump/GLBlockJump/ConnectServer.h
@@ -13,3 +13,4 @@ void CleanupSocket(SOCKET sock);
 void RecvInitPlayers(SOCKET sock, UINT &MyID);
 void RecvInitWorldStatic(SOCKET sock);
 void RecvInitWorldDynamic(SOCKET sock);
+void SendInputChange(SOCKET sock, const PlayerInputs& input);
