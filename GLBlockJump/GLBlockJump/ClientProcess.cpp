0f5f47c94ca334eb4b07b7e16cac1c3065f8b59e
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-11-17 06:45:53 +0900

    클라 SendInputChange 적용

--- GLBlockJump/GLBlockJump/ClientProcess.cpp
+++ GLBlockJump/GLBlockJump/ClientProcess.cpp
@@ -1,10 +1,21 @@
 #include "ClientProcess.h"
+#include "ConnectServer.h"
+#include "Object.h"
+#include "Player.h"
 
 
 DWORD WINAPI ClientProcess(LPVOID arg)
 {
+    extern UINT MyID;
 	//월드 정보를 수신, 입력 정보를 송신
+    SOCKET param = (SOCKET)arg;
 
+    while (1)
+    {
+        RecvWorld(param);
+        SendInputChange(param, players[MyID].inputs);
+    }
 
+    return 0;
 	return 0;
 }
