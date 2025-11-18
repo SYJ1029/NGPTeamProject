9125ebbd7237002ea5c789a114b995b0eb79a595
SYJ1029 <abg0986@gmail.com>
2025-11-18 14:08:29 +0900

    SendWorld 완전 구현
    종합 디버깅 과정에서 생긴 오류 1차 수정

--- GLBlockJump/GLBlockServer/ServerProcess.cpp
+++ GLBlockJump/GLBlockServer/ServerProcess.cpp
@@ -6,9 +6,22 @@ PlayerInputs pi[MAX_CLIENTS];
 
 void SendWorld(ThreadParam* param)
 {
+
     // 월드의 정보를 패킷에 담아 전송한다.
 
+    PacketParam pkHeader;
+
+    pkHeader.type = PACK_FRAME_STATE;
+	// 전송할 패킷의 크기를 계산한다
     
+    std::vector<uint8_t> tempData = Fs.Serialize();
+	pkHeader.size = sizeof(PacketParam) + tempData.size();
+
+	// 패킷 헤더 전송
+    send(param->sock, (char*)&pkHeader, sizeof(PacketParam), 0);
+    // 패킷 본문 전송
+    std::vector<uint8_t> serializedData = Fs.Serialize();
+	send(param->sock, (char*)serializedData.data(), serializedData.size(), 0);
 }
 
 
@@ -23,7 +36,9 @@ DWORD WINAPI ServerProcess(LPVOID arg)
 
     while (1)
     {
+		// 매 틱마다 월드 정보를 전송
         SendWorld(param);
+		// 클라이언트로부터 입력 정보 수신
     }
 
     return 0;
