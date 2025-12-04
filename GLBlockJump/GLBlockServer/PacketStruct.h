d8b6b195aadc2e5b3fc858acefb933b558ae8348
SYJ1029 <abg0986@gmail.com>
2025-12-04 14:01:58 +0900

    constexpr을 써보자

--- GLBlockJump/GLBlockServer/PacketStruct.h
+++ GLBlockJump/GLBlockServer/PacketStruct.h
@@ -55,7 +55,7 @@ struct PlayerInitInfo {
 
 struct PktInitPlayers {
     int myPlayerId;
-    PlayerInitInfo players[3]; // 모든 플레이어의 초기 위치
+    PlayerInitInfo players[MAX_PLAYER]; // 모든 플레이어의 초기 위치
     PktInitPlayers() {}
     ~PktInitPlayers() {}
 };
@@ -74,7 +74,7 @@ struct PlayerSyncData {
 };
 struct PktFrameState {
     Game_State gameState; // 지금 게임이 RUNNING인지 FINISHED인지도 함께 보냄  
-    PlayerSyncData players[3];
+    PlayerSyncData players[MAX_PLAYER];
     int winnerId = -1;
 
     int move_block_size;
